17975c4b14c3f3346b70a2471f476fa52f29a3f3
Henrique Fidellis <fidellis@bb.com.br>
2021-09-10 10:37:35 -0300

    alt

--- src/client/lib/constants.js
+++ src/client/lib/constants.js
@@ -1,6 +1,5 @@
 export const PROFILE_URL = 'https://connections.bb.com.br/profiles/html/profileView.do?uid=';
 export const ARQUIVO_URL = 'https://diemp2.intranet.bb.com.br/arquivos/api/arquivo';
-// export const AVATAR_URL = '//disem.intranet.bb.com.br/avatar/';
 export const AVATAR_URL = 'https://humanograma.intranet.bb.com.br/avatar/';
 export const HUMANOGRAMA_URL = 'https://humanograma.intranet.bb.com.br';
 export const CORREIO_URL = 'https://outlook.office.com/mail/inbox';
