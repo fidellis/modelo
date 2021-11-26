d403e3d5047b5cb1e4ab477d79e32ad48693e1f0
Henrique Fidellis <fidellis@bb.com.br>
2021-11-26 12:51:11 -0300

    alt

--- src/server/ccmei.js
+++ src/server/ccmei.js
@@ -75,7 +75,7 @@ async function get(page, row, i) {
 }
 
 async function start() {
-    const browser = await puppeteer.launch({ headless: false, slowMo: 100 });
+    const browser = await puppeteer.launch({ headless: true, slowMo: 100 });
     const page = await browser.newPage();
     await page.goto('https://consopt.www8.receita.fazenda.gov.br/consultaoptantes');
 
