480014ec70053744d3d7d1fec48ebdf490b26e0a
Henrique Fidellis <fidellis@bb.com.br>
2022-05-02 14:48:48 -0300

    alt

--- src/client/components/form/Form.jsx
+++ src/client/components/form/Form.jsx
@@ -60,7 +60,7 @@ class Formulario extends Component {
     return (
 
       <div style={{ ...styles.container, width }}>
-        {/* {showLoading && <Loading />} */}
+        {showLoading && <Loading />}
         <Form
           {...props}
           isValid={v => this.setState({ isValid: v }, () => (isValid ? isValid(v) : null))}
@@ -91,4 +91,4 @@ Formulario.defaultProps = {
 };
 
 const mapStateToProps = ({ app: { showLoading } }) => ({ showLoading });
-export default connect(mapStateToProps, { msg })(Formulario);
+export default connect(() => { }, { msg })(Formulario);
