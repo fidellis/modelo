51557caf272396ce17dcada94dbb2f491cc2dcb6
Henrique Fidellis <fidellis@bb.com.br>
2022-05-02 14:51:39 -0300

    alt

--- src/client/components/form/Form.jsx
+++ src/client/components/form/Form.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Form from './form/Form';
 import Button from '~/components/Button';
-import Loading from '~/components/Loading';
+// import Loading from '~/components/Loading';
 import { connect } from 'react-redux';
 
 export const msg = txt => dispatch => dispatch({ type: 'SUCCESS', msg: txt });
@@ -60,7 +60,7 @@ class Formulario extends Component {
     return (
 
       <div style={{ ...styles.container, width }}>
-        {showLoading && <Loading />}
+        {/* {showLoading && <Loading />} */}
         <Form
           {...props}
           isValid={v => this.setState({ isValid: v }, () => (isValid ? isValid(v) : null))}
